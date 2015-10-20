857a2916c2f22aba630718dce6dd22c70885ad95
Gustavo B Paterno <paternogbc@gmail.com>
2015-10-20 16:00:25 -0300

    initial commit.

---
+++ R/1_pacotes_necessarios.R
@@ -0,0 +1,11 @@
+### Pacotes necessários---------------------------------------------------------
+
+# Instalação:
+install.packages("ape")
+install.packages("phytools")
+install.packages("picante")
+install.packages("caper")
+install.packages("phylolm")
+install.packages("devtools")
+devtools::install_github("paternogbc/sensiPhy")
+################################################################################
