e5572ea685db224ce7c417a1c8f35764838d444a
Gustavo B Paterno <paternogbc@gmail.com>
2015-10-23 15:33:50 -0300

    update

--- R/2_manipular_arvores_filogeneticas.R
+++ R/2_manipular_arvores_filogeneticas.R
@@ -26,10 +26,17 @@ is.ultrametric(arvore)
 # Como verificar se a árvore é enraizada?
 is.rooted(arvore)
 
+# Cria nomes para os nodes da árvore (nós)
+arvore <- makeNodeLabel(arvore) 
+arvore$node.label
+
 # Como remover uma/várias espécies da árvore? ----------------------------------
 drop.tip(arvore, arvore$tip.label[1])   # remove a primeira espécies da árvore
 drop.tip(arvore, arvore$tip.label[1:5]) # remove as cinco primeiras espécies
 drop.tip(arvore, "Tringa_glareola")     # remove a espécies "Tringa_glareola"
+
+# Como remover um clado da árvore (nó)?
+extract.clade(phy = arvore, node = "Node61") # remove o nó 61
 
 # Depois de remover as espécies que deseja, você pode salvar a árvore em um objeto:
 arvore2 <- drop.tip(arvore, "Tringa_glareola")
@@ -38,7 +45,28 @@ arvore2   # perceba que agora a árvore possui 70 espécies (em vez de 71)
 # Para exportar a árvore em formato Newwick, utilize a função 'write.tree()'
 write.tree(phy = arvore2, file = "arvore2.tre")
 
+# para excluir o arquivo
+unlink("arvore2.tre")
+
 # Como visualizar a árvore (básico)---------------------------------------------
 plot(arvore, cex = .5)                # cex regula o tamanho da fonte 
-plot(arvore, show.tip.label = F)      # esconde os nomes
+plot(arvore, show.tip.label = F)      # esconde os nomes as espécies
+plot(arvore, show.node.label = T,     # mostra o nome dos nós (nodes)
+     show.tip.label = F, cex = .7)
 plot(arvore, type = "fan", cex = .5)  # Plota uma árvore circular
+
+### Como criar um árvore filogenética manualmente no R?-------------------------
+### Formato Newwick: 
+sp <- "((specie_A:5, specie_B:5):15, especie_C:20);"
+
+### Exporta a árvore:
+cat(sp, file = "exemplo.tre", sep = "\n")
+### Carrega a árvore:
+arv.exemplo <- read.tree("exemplo.tre")
+arv.exemplo
+plot(arv.exemplo)
+
+### Deleta arvore do diretório:
+unlink("exemplo.tre")
+
+### FIM-------------------------------------------------------------------------
