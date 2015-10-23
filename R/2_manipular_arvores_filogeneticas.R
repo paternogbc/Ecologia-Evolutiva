a89ad239b7c4972e58105420006b26742d62c58a
Gustavo B Paterno <paternogbc@gmail.com>
2015-10-23 17:19:28 -0300

    update

--- R/2_manipular_arvores_filogeneticas.R
+++ R/2_manipular_arvores_filogeneticas.R
@@ -7,13 +7,22 @@ library(ape)
 
 # Como carregar um árvore filogenética no R? -----------------------------------
 # A função 'read.tree()' lê árvores no formato (Newick)
-arvore <- read.tree("filogenia/tree.tre")
+arvore <- read.tree("filogenia/arvore.tre")
 
 # Confira se a árvore foi carregada corretamente (número de espécies,
 # número de nós, nome das espécies, se a árvore é enraizada e se
 # possui informações sobre o comprimento dos ramos)
 arvore
 
+# Como visualizar a árvore (básico)---------------------------------------------
+plot(arvore, cex = .5)                # cex regula o tamanho da fonte 
+plot(arvore, show.tip.label = F)      # esconde os nomes as espécies
+plot(arvore, show.node.label = T,     # mostra o nome dos nós (nodes)
+     show.tip.label = F, cex = .7)
+plot(arvore, type = "fan", cex = .5)  # Plota uma árvore circular
+###-----------------------------------------------------------------------------
+
+### Características da sua árvore:----------------------------------------------
 # Veja a lista de espécies da sua árvore:
 arvore$tip.label
 
@@ -26,21 +35,37 @@ is.ultrametric(arvore)
 # Como verificar se a árvore é enraizada?
 is.rooted(arvore)
 
-# Cria nomes para os nodes da árvore (nós)
+# Se a árvore não tiver nome nos nós (nodes), utilize essa função para criar
+# nomes para os nós
+arvore$node.label
 arvore <- makeNodeLabel(arvore) 
+
+# Veja que agora os nós possuem um nome:
 arvore$node.label
 
-# Como remover uma/várias espécies da árvore? ----------------------------------
+# Existem politomias na minha árvore?
+# Se a sua árvore não tiver politomias, o número de nós dever ser igual ao
+# número de espécies -1. Se isso não ocorrer, sua árvore possue politomias:
+Nnode <- arvore$Nnode               # Número de nós
+Nsp   <- length(arvore$tip.label)   # Número de espécies
+# Existem politomias na minha árvore?
+Nnode == (Nsp -1)
+
+# Quantas politomias existem na minha árvore?
+(Nsp -1) - Nnode
+###-----------------------------------------------------------------------------
+
+### Como remover uma/várias espécies da árvore? --------------------------------
 drop.tip(arvore, arvore$tip.label[1])   # remove a primeira espécies da árvore
 drop.tip(arvore, arvore$tip.label[1:5]) # remove as cinco primeiras espécies
 drop.tip(arvore, "Tringa_glareola")     # remove a espécies "Tringa_glareola"
 
-# Como remover um clado da árvore (nó)?
+# Como cortar um node da árvore (nó)?
 extract.clade(phy = arvore, node = "Node61") # remove o nó 61
 
 # Depois de remover as espécies que deseja, você pode salvar a árvore em um objeto:
-arvore2 <- drop.tip(arvore, "Tringa_glareola")
-arvore2   # perceba que agora a árvore possui 70 espécies (em vez de 71)
+arvore2 <- drop.tip(arvore, c("Tringa_glareola", "Haematopus_ostralegus"))
+arvore2   # perceba que agora a árvore possui 69 espécies (em vez de 71)
 
 # Para exportar a árvore em formato Newwick, utilize a função 'write.tree()'
 write.tree(phy = arvore2, file = "arvore2.tre")
@@ -48,14 +73,7 @@ write.tree(phy = arvore2, file = "arvore2.tre")
 # para excluir o arquivo
 unlink("arvore2.tre")
 
-# Como visualizar a árvore (básico)---------------------------------------------
-plot(arvore, cex = .5)                # cex regula o tamanho da fonte 
-plot(arvore, show.tip.label = F)      # esconde os nomes as espécies
-plot(arvore, show.node.label = T,     # mostra o nome dos nós (nodes)
-     show.tip.label = F, cex = .7)
-plot(arvore, type = "fan", cex = .5)  # Plota uma árvore circular
-
-### Como criar um árvore filogenética manualmente no R?-------------------------
+### Como criar um árvore filogenética manualmente?------------------------------
 ### Formato Newwick: 
 sp <- "((specie_A:5, specie_B:5):15, especie_C:20);"
 
@@ -70,3 +88,4 @@ plot(arv.exemplo)
 unlink("exemplo.tre")
 
 ### FIM-------------------------------------------------------------------------
+
